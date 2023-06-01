58abfb830c174d1d9eb1b3ce73812bf51b35f436
Saleem Abdulrasool <compnerd@compnerd.org>
2023-05-31 18:14:08 -0700

    Tests: replace `sleep` with `Thread.sleep(forTimeInterval:)`
    `sleep` is not portable as it is not a standard C function.  Use the
    Foundation function to provide a single codepath across all the targets.

--- Tests/SwiftDocCTests/Utility/SynchronizationTests.swift
+++ Tests/SwiftDocCTests/Utility/SynchronizationTests.swift
@@ -65,7 +65,7 @@ class SynchronizationTests: XCTestCase {
 
         // Schedule the synchronized block of work asynchronously
         testQueue.async {
-            synced.sync { _ in _ = sleep(5) }
+            synced.sync { _ in Thread.sleep(forTimeInterval: 5) }
         }
         
         // Asynchronously perform a check after 0.25 secs that the lock is locked
@@ -116,7 +116,7 @@ class SynchronizationTests: XCTestCase {
         // Block the access for a second, then update to `true`
         testQueue.async {
             synced.sync {
-                _ = sleep(1)
+                Thread.sleep(forTimeInterval: 1)
                 $0 = true
             }
         }
@@ -157,7 +157,7 @@ class SynchronizationTests: XCTestCase {
         // Block the access for a second, then update to `true`
         testQueue.async {
             synced.sync {
-                _ = sleep(5)
+                Thread.sleep(forTimeInterval: 5)
                 value = true
             }
         }
